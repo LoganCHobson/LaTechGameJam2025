a10b60c356a2d07287f7b8ba49e0530c0bfef9ed
Milo <milohatten@gmail.com>
2025-04-11 21:28:26 -0500

    More updates to scripts

--- Assets/Scripts/ColorChange.cs
+++ Assets/Scripts/ColorChange.cs
@@ -2,17 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ColorChange : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class ColorChange : MonoBehaviour{
+
+    bool color1;
+    bool color2;
+    bool color3;
+    bool complete;
+    ShaderColors color;
+
+    public enum ShaderColors {
+        BLACKANDWHITE,
+        RED,
+        GREEN,
+        BLUE,
+        FULL,
+    }
+    
+    void Start(){
+        color = ShaderColors.BLACKANDWHITE;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        color1 = Eye1.isGrabbed;
+        if (color1){
+            color1 = false;
+            Eye1.clearGrab();
+            ChangeColor(ShaderColors.RED);
+        }
+        else if (color2){
+            color2 = false;
+
+            ChangeColor(ShaderColors.GREEN);
+        }
+        else if (color3){
+            color3 = false;
+
+            ChangeColor(ShaderColors.BLUE);
+        }
+
     }
+    public void ChangeColor(ShaderColors newColor){
+        if (color != newColor){
+            color = newColor;
+        }
+
+        //changing the colors below
+        if (color == ShaderColors.RED){
+
+        }
+        else if (color == ShaderColors.GREEN){
+
+        }
+        else if (color == ShaderColors.BLUE){
+
+        }
+        else if (color == ShaderColors.FULL){
+            
+        }
+
+    }
+
 }
--- Assets/Scripts/Eye1.cs
+++ Assets/Scripts/Eye1.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class Eye1 : MonoBehaviour{
 
-    public bool isGrabbed = false;
+    public static bool isGrabbed = false;
     public GameObject eye;
 
     void OnTriggerEnter2D(Collider2D collision){
         if (collision.CompareTag("Player")){
+            Debug.Log("ceye 1 grabbed");
             eye.SetActive(false);
             isGrabbed = true;
         }
     }
 
-    
+    public static void clearGrab(){
+        isGrabbed = false;
+    }
 
 }
