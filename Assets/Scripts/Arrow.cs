c97268e11fdfb47c06370dac4ac9c3521fd6cd43
Christian-Hal <chall.monkey@outlook.com>
2025-04-13 14:37:16 -0700

    Puzzle assets

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Arrow : Item
 {
-    // Start is called before the first frame update
     public override void PickedUp()
     {
         throw new System.NotImplementedException();
+    }
+    public void Update()
+    {
+        
     }
 }
