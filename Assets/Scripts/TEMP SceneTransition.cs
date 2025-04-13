a5d92f624254b61dda47248b2edfeab3383b5d38
Christian-Hal <chall.monkey@outlook.com>
2025-04-12 20:57:53 -0700

    Prefabs + teleport + script functionality
    I dont know what is left for me to do...

--- Assets/Scripts/TEMP SceneTransition.cs
+++ Assets/Scripts/TEMP SceneTransition.cs
@@ -1,6 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,27 +6,31 @@ public class TEMPSceneTransition : MonoBehaviour
 {
     public Animator transition;
     public float transitionTime = 1f;
+    public GameObject player;
+    public Vector3 teleportLocation = new Vector3(0, 0, 0);
     // Start is called before the first frame update
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("this is working");
-            LoadNextLevel();
+            LoadNextArea();
         }
     }
 
-    public void LoadNextLevel()
+    public void LoadNextArea()
     {
-        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+        StartCoroutine(LoadArea(SceneManager.GetActiveScene().buildIndex + 1));
     }
 
-    IEnumerator LoadLevel(int levelIndex)
+    IEnumerator LoadArea(int levelIndex)
     {
         transition.SetTrigger("ChangeScene");
 
         yield return new WaitForSeconds(transitionTime);
+        player.transform.position = teleportLocation;
+        yield return new WaitForSeconds(transitionTime);
 
-        SceneManager.LoadScene(levelIndex);
+        transition.SetTrigger("ExitChange");
+
     }
 }
