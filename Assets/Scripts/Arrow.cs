57dc6de0e40da021535020a1f791cd986b835561
Christian-Hal <chall.monkey@outlook.com>
2025-04-13 13:28:50 -0700

    Forest Puzzle minus ground for it

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : Item
+{
+    // Start is called before the first frame update
+    public override void PickedUp()
+    {
+        throw new System.NotImplementedException();
+    }
+}
