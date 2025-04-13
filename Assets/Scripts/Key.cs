826b9171541c4e515ea3bc4050f9c526a75ae461
Logan <Loganhobson31@gmail.com>
2025-04-13 11:14:07 -0500

    Small player adjustment. Keys now have dialog

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 
 public class Key : Item
 {
+    public string dialog;
     public override void PickedUp()
     {
         Inventory.Instance.AddItem(this);
+        DialogController.Instance.Dialog(dialog);
         //Do thing I guess?
         Debug.Log("Picked up");
 
